718efb791337b32438b603a43d9c035ce83a1671
Makaosva <maka.osses@gmail.com>
2024-05-24 21:35:59 -0400

    feat/Arreglo cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ function App() {
   const perritos = [
     {
       imagen: "",
-      title: "Perrito 1",
+      name: "Perrito 1",
       desciption: "Description 1",
       tags: [{ text: "texto 1", color: "success" }],
     },
     {
       imagen: "",
-      title: "Perrito 2",
+      name: "Perrito 2",
       desciption: "Description 2",
       tags: [{ text: "texto 1", color: "info" }],
     },
     {
       imagen: "",
-      title: "Perrito 3",
+      name: "Perrito 3",
       desciption: "Description 3",
-      tags: [{ text: "texto 1", color: "success" }],
+      tags: [{ text: "texto 1", color: "primary" }],
     },
     {
       imagen: "",
-      title: "Perrito 4",
+      name: "Perrito 4",
       desciption: "Description 4",
       tags: [{ text: "texto 1", color: "danger" }],
     },
--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import Tags from "./Tags";
 
-function MyCard(imagen, name, description, tags) {
+function MyCard({ imagen, name, description, tags }) {
   return (
     <div className="card">
       <img src={imagen} alt={name} className="card-img-top" />
       <div className="card-body">
-        <h4 className="card-title">{title}</h4>
+        <h4 className="card-title">{name}</h4>
         <p className="card-text">{description}</p>
         <Tags tags={tags} />
       </div>
