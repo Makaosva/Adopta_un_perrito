c16db1695d7b21ff8a6ef3abe87f093fe8f092a1
Makaosva <maka.osses@gmail.com>
2024-05-24 20:09:51 -0400

    feat/Agregar card en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
+import MyCard from "./components/MyCard";
 
 function App() {
   const perritos = [
@@ -27,9 +25,19 @@ function App() {
       desciption: "Description 4",
     },
   ];
+
   return (
     <>
       <Header title="Adopta un perrito" />
+      <div className="container mt-4">
+        <div className="row">
+          {perritos.map((libros, index) => (
+            <div className="col-md-4" key={index}>
+              <MyCard {...perritos} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
